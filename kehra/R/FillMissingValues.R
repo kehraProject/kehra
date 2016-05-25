cdc3b037fd3d55f0a719514015622780ca8e78f3
Claudia Vitolo <claudia.vitolo@gmail.com>
2016-05-25 17:19:49 +0100

    Fixed FillMissingValues to accept result from GetDataFromECMWF

--- kehra/R/FillMissingValues.R
+++ kehra/R/FillMissingValues.R
@@ -2,56 +2,54 @@
 #'
 #' @description Fill missing values
 #'
-#' @param df dataframe containing the timeseries in columns separated by ID
-#' 
+#' @param df dataframe containing the timeseries in columns separated by ID (Header must follow this convention: column 1 = "datetime", column 2 = "SiteID", column 3 = "variable name"). df can be the result of GetDataFromECMWF().
+#' @param maxgap maximum gap to interpolate (e.g. 6 hours)
+#'
 #' @return updated df with infilled values
 #'
 #' @export
 #'
 #' @examples
-#' # FillMissingValues(clima)
+#' # FillMissingValues(clima, maxgap = 6)
 #'
 
-FillMissingValues <- function(df){
-  
-  # Load the training set.
-  # df <- readRDS("~/Documents/training.rds")
+FillMissingValues <- function(df, maxgap){
+
   # library(dplyr)
   # library(xts)
   # library(zoo)
-  
+  # df <- clima
+  # site <- as.character(unique(df$SiteID))[1]
+
+  newDF <- data.frame(matrix(NA, nrow = 0, ncol= 3))
+
   # For each site create a time series
-  for (site in as.character(unique(df$SiteID))[1:3]){
-    
+  for (site in as.character(unique(df$SiteID))){
+
     print(site)
-    
+
     siteROWS <- which(df$SiteID == site)
     dfg <- df[siteROWS, ]
-    
-    datetime <- as.POSIXct(paste(paste(dfg$Year, dfg$Month, dfg$Day, 
-                                       sep = "-"), 
-                                 paste(dfg$Hour, ":00", sep="")), 
-                           format = "%Y-%m-%d %H:%M")
-    
-    for (column in 13:26) {
-      
-      siteDF <- xts(dfg[,column], order.by = datetime)
-      
-      if (column %in% c(13, 14, 15, 19, 20)) {
-        x <- na.approx(siteDF, maxgap = 6, na.rm = FALSE)
-      }
-      
-      if (column %in% c(16:18, 21:26)) {
-        x <- na.approx(siteDF, maxgap = 12, na.rm = FALSE)
-      }
-      
-      df[siteROWS, column] <- coredata(x)
-      
-    }
-    
+
+    datetime <- seq.POSIXt(as.POSIXlt(head(dfg$datetime, n = 1)),
+                           as.POSIXlt(tail(dfg$datetime, n = 1)),
+                           by = "hour")
+    emptyTS <- xts(rep(NA, length(datetime)), order.by = datetime)
+
+    siteDF <- xts(dfg[, 3], order.by = as.POSIXlt(dfg$datetime))
+    siteDFextented <- merge(emptyTS, siteDF)[,2]
+
+    x <- na.approx(object = siteDFextented, maxgap = maxgap, na.rm = FALSE)
+
+    newDF <- rbind(newDF, data.frame(cbind(index(x),
+                                           rep(site, length(x)),
+                                           coredata(x))))
+
   }
-  
-  return(df)
-  
+
+  names(newDF) <- names(df)
+
+  return(newDF)
+
 }
 
