b5e52374c7b8f29fdc9a3b222ae9869ee56e53f0
Claudia Vitolo <claudia.vitolo@gmail.com>
2016-06-14 10:10:42 +0100

    First part of EM algorithm

--- scripts/AirPollutionBN/Model.R
+++ scripts/AirPollutionBN/Model.R
@@ -16,16 +16,18 @@
 # install.packages("bnlearn")
 library(devtools)
 install_github("cvitolo/bnlearn")
+
+# load parallel and bnlearn and rsprng.
+library(parallel)
 library(bnlearn)
+cl <- makeCluster(detectCores()-1)
+# check it works.
+# clusterEvalQ(cl, runif(10))
 
 # Load the training set
-df <- readRDS("~/data/training.rds")
-as.data.frame(names(df))
-df <- df[,c(2:24,28)]
-# Check data structure
-str(df)
-# which colum does not contain NA?
-colSums(is.na(df)) == 0
+df <- readRDS("~/data/training.rds") # as.data.frame(names(df))
+df <- df[,c(2:24,28)] # Check data structure with str(df)
+# which colum does not contain NA? colSums(is.na(df)) == 0
 # remove NA from weather variables
 df <- df[-which(is.na(df$ws)),]
 # remove NA from health variables
@@ -39,34 +41,10 @@ df <- droplevels(df[,c("Region", "Zone", "Type", "Year", "Season", "Month",
 
 # Imputing Missing Data With Expectation – Maximization
 # initialise using the empty graph and complete observations.
-library(data.table)
-dfc <- data.table(df[complete.cases(df),])
+dfc <- df[complete.cases(df), ]
 
 dag <- empty.graph(names(dfc)); graphviz.plot(dag)
-bn <- bn.fit(dag, dfc)
-
-current <- df
-for (i in 19:24) {
-  
-  colName <- names(current)[i]
-  colValues <- eval(parse(text = paste("current$", colName, sep = ""))) 
-  
-  # E: replacing missing values with their (E)xpectation.
-  current[is.na(colValues), colName] <- predict(object = bn, 
-                                                node = colName, 
-                                                data = current[is.na(colValues),
-                                                               names(current)[1:(i-1)]],
-                                                method = "bayes-lw")
-  
-  # M: learning the model that (M)aximises the score with the current data.
-  dag <- mmhc(current[complete.cases(current),])
-  bn <- bn.fit(dag, current[complete.cases(current),])
-  
-  # graphviz.plot(dag)
-  readline(prompt="Press [enter] to continue")
-  
-}
-
+bn <- bn.fit(dag, dfc)                                       # node.ordering(bn)
 
 # Define blacklist
 bl <- data.frame("from" = c(rep("Region",10),
@@ -92,9 +70,61 @@ bl <- data.frame("from" = c(rep("Region",10),
                           "Region", "Zone", "Type", "Year", "Season", "Month", "Day", "Hour", "Latitude", "Altitude",
                           "Region", "Zone", "Type", "Year", "Season", "Month", "Day", "Hour", "Latitude", "Longitude"))
 
-dag <- mmhc(df00, blacklist = bl, alpha = 0.05)
-any(is.null(df00))
-unique(df00$Longitude)
+# for (j in 1:100){ # check convergence at each loop
+rmse <- c()
+current <- df
+for (i in 24:19) { # i <- 24
+  
+  # what is the name of the column to fill in?
+  colName <- names(current)[i]
+  print(colName)
+  # extract the set of complete observations (ignoring only column i)
+  completeRows <- which(complete.cases(current[,-i]))
+  dfc <- current[completeRows, ]
+  # within the above table, what are the values of column i?
+  colValues <- eval(parse(text = paste("current$", colName, 
+                                       "[completeRows]", sep = "")))
+  # in what rows we have NA?
+  naRows <- which(is.na(colValues))
+  
+  # If there are columns in which the non-zero-probability levels do not
+  # contain
+  col2remove <- c()
+  for (nCol in names(which(sapply(dfc, class) == 'factor'))){
+    stringCol <- paste("bn$", nCol, "$prob", sep = "")
+    levelsCol <- names(which( eval(parse(text = stringCol)) != 0 ))
+    if (!all(unique(current[,nCol]) %in% levelsCol)) {
+      col2remove <- c(col2remove, nCol) 
+    }
+  }
+  col2remove <- which(names(current) %in% col2remove)
+  
+  # E: replacing missing values with their (E)xpectation
+  E <- predict(object = bn, 
+               node = colName, 
+               data = dfc[,-c(col2remove,i)],
+               method = "bayes-lw")
+  measured <- current[completeRows[-naRows], colName]
+  modelled <- E[-naRows]
+  rmse <- c(rmse, sqrt( mean( (modelled-measured)^2 , na.rm = TRUE ) ))
+  current[completeRows[naRows], colName] <- E[naRows]
+  
+  # M: learning the model that (M)aximises the score with the current data.
+  currentComplete <- current[complete.cases(current),]
+  # call a learning function passing the cluster object (the return value of the 
+  # previous makeCluster() call) as a parameter.
+  dag <- hc(currentComplete, blacklist = bl)
+  bn <- bn.fit(dag, currentComplete)
+  
+  graphviz.plot(dag)
+  readline(prompt="Press [enter] to continue") 
+  
+}
+
+# }
+
+# stop the cluster.
+stopCluster(cl)
 
 graphviz.plot(dag, highlight = NULL, layout = "dot",
               shape = "circle", main = NULL, sub = NULL)
